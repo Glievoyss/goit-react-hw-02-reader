dc303b890dee1e9b31d3799498b109c1352a62cc
Glievoyss <glievoy.s@gmail.com>
2019-10-08 13:38:12 +0300

    hw react 2-1

---
+++ src/components/Publication/Publication.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../Reader/Reader.module.css';
+
+const Publication = ({ item, counter }) => {
+  return (
+    <article className={styles.publication}>
+      <h2>
+        {counter}.{item.title}
+      </h2>
+      <p>{item.text}</p>
+    </article>
+  );
+};
+
+Publication.propTypes = {
+  item: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  counter: PropTypes.number.isRequired,
+};
+
+export default Publication;
---
+++ src/components/Reader/Reader.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import styles from './Reader.module.css';
+import Controls from '../Controls/Controls';
+import Counter from '../Counter/Counter';
+import Publication from '../Publication/Publication';
+
+export default class Reader extends Component {
+  static defaultProps = {
+    counter: 1,
+  };
+
+  state = {
+    counter: this.props.counter,
+  };
+
+  increaseCounter = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter - 1,
+    }));
+  };
+
+  reduceCounter = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  render() {
+    const { items } = this.props;
+    const maxCounter = items.length;
+    const { counter } = this.state;
+    const currentItem = items[counter - 1];
+
+    return (
+      <div className={styles.reader}>
+        <Controls
+          increaseCounter={this.increaseCounter}
+          reduceCounter={this.reduceCounter}
+          counter={counter}
+          maxCounter={maxCounter}
+        />
+        <Counter counter={counter} maxCounter={maxCounter} />
+        <Publication item={currentItem} counter={counter} />
+      </div>
+    );
+  }
+}
